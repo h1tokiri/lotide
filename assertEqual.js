0e869651c35f32d9eaf158929084f290660863ec
Javin Leslie Lau <javinlau21@gmail.com>
2024-10-01 19:29:05 -0400

    v3 with the eslint actually employed

--- assertEqual.js
+++ assertEqual.js
@@ -1,6 +1,6 @@
-const assertEqual = function (actual, expected) {
-const stopSign = "\u{1F6D1}"
-const greenCheck = "\u{2705}"
+const assertEqual = function(actual, expected) {
+  const stopSign = "\u{1F6D1}";
+  const greenCheck = "\u{2705}";
 
 
   if (actual === expected) {
