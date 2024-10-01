4431bc36b54560102be0de59da300f1e7d81dd1a
Javin Leslie Lau <javinlau21@gmail.com>
2024-10-01 19:21:44 -0400

    assertEqual assignment

---
+++ assertEqual.js
@@ -0,0 +1,15 @@
+const assertEqual = function (actual, expected) {
+const stopSign = "\u{1F6D1}"
+const greenCheck = "\u{2705}"
+
+
+  if (actual === expected) {
+    console.log(`${greenCheck} Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`${stopSign} Assertion Failed: ${actual} !== $expected}`);
+  }
+};
+
+//test code
+assertEqual("Lighthouse Labs", "Bootcamp");
+assertEqual(1, 1);
