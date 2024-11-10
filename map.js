ebe77fbb71f81c617cb4473daf580d81d1748eb6
Javin Leslie Lau <javinlau21@gmail.com>
2024-11-10 16:47:25 -0500

    v1 map.js

---
+++ map.js
@@ -0,0 +1,48 @@
+const stopSign = "\u{1F6D1}"; // keeping my emojis
+const greenCheck = "\u{2705}";
+
+// eqArrays function that was copied over
+const eqArrays = function(arr1, arr2) {
+  if (arr1.length !== arr2.length) {
+    return false;
+  }
+
+  for (let i = 0; i < arr1.length; i++) {
+    if (arr1[i] !== arr2[i]) {
+      return false;
+    }
+  }
+  return true;
+};
+
+const assertArraysEqual = function(arr1, arr2) {
+  //check if the arrays are equal using eqArrays
+  if (eqArrays(arr1, arr2)) {
+    console.log(`${greenCheck} Assertion Passed: ${arr1} === ${arr2}`);
+  } else {
+    console.log(`${stopSign} Assertion Failed: ${arr1} !== ${arr2}`);
+  }
+};
+
+const map = function (array, callback) {
+  const results = [];
+  for (let item of array) {
+    // console.log("item BEFORE: ", item);
+    // console.log("item AFTER: ", callback(item));
+    // console.log("---");
+    results.push(callback(item));
+  }
+  return results;
+};
+
+const words = ["ground", "control", "to", "major", "tom"];
+
+const results1 = map(words, (word) => word[0]);
+console.log(results1);
+assertArraysEqual(results1, ['g', 'c', 't', 'm', 't']);
+
+const results2 = map(words, (word) => word.toUpperCase());
+assertArraysEqual(results2, ['GROUND', 'CONTROL', 'TO', 'MAJOR', 'TOM']);
+
+const results3 = map(words, (word) => word.length);
+assertArraysEqual(results3, [6, 7, 2, 5, 3]);
