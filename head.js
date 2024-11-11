78bdf8ba26ff4dbe446c701aacdbeb285a70e1f5
Javin Leslie Lau <javinlau21@gmail.com>
2024-11-10 20:39:45 -0500

    v2 head.js

--- head.js
+++ head.js
@@ -1,21 +1,19 @@
-const assertEqual = function(actual, expected) {
-  const stopSign = "\u{1F6D1}";
-  const greenCheck = "\u{2705}";
+// const assertEqual = function(actual, expected) {
+//   const stopSign = "\u{1F6D1}";
+//   const greenCheck = "\u{2705}";
 
 
-  if (actual === expected) {
-    console.log(`${greenCheck} Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`${stopSign} Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
+//   if (actual === expected) {
+//     console.log(`${greenCheck} Assertion Passed: ${actual} === ${expected}`);
+//   } else {
+//     console.log(`${stopSign} Assertion Failed: ${actual} !== ${expected}`);
+//   }
+// };
 
+const assertEqual = require("./assertEqual");
 
 const head = function(array) {
   return array[0];
 };
 
-assertEqual(head(["Lighthouse Labs", "Web Development"]), "Bootcamp");
-assertEqual(head([1,2,3,4,5]), 1);
-assertEqual(head([1]),1);
-assertEqual(head([]), undefined);
+module.exports = head;
