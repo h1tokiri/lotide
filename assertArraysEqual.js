f3e2e3e859382128dddf6922d034be9cb4b9be83
Javin Leslie Lau <javinlau21@gmail.com>
2024-11-10 21:03:34 -0500

    v3 assertArraysEqual.js

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,19 +1,7 @@
 const stopSign = "\u{1F6D1}"; // keeping my emojis
 const greenCheck = "\u{2705}";
 
-// eqArrays function that was copied over
-const eqArrays = function(arr1, arr2) {
-  if (arr1.length !== arr2.length) {
-    return false;
-  }
-
-  for (let i = 0; i < arr1.length; i++) {
-    if (arr1[i] !== arr2[i]) {
-      return false;
-    }
-  }
-  return true;
-};
+const eqArrays = require("./eqArrays");
 
 const assertArraysEqual = function(arr1, arr2) {
   //check if the arrays are equal using eqArrays
