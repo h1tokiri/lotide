7b7e5b5b477c042b6d72b0020b45cd02ecc9f8b3
Javin Leslie Lau <javinlau21@gmail.com>
2024-11-10 21:01:11 -0500

    v1 assertArraysEqualTest.js

---
+++ test/assertArraysEqualTest.js
@@ -0,0 +1,7 @@
+const assertEqual = require("../assertEqual");
+const assertArraysEqual = require("../assertArraysEqual");
+
+assertArraysEqual([1, 2, 3], [1, 2, 3]); // Should pass
+assertArraysEqual([1, 2, 3], [3, 2, 1]); // Should fail
+assertArraysEqual(["1", "2", "3"], ["1", "2", "3"]); // Should pass
+assertArraysEqual(["1", "2", "3"], ["1", "2", 3]); // Should fail
