db0b5708fbcd7bc261619e7836bf61a0cef92bbc
Javin Leslie Lau <javinlau21@gmail.com>
2024-11-10 21:22:10 -0500

    v3 headTest.js

--- test/headTest.js
+++ test/headTest.js
@@ -1,7 +1,20 @@
 const assertEqual = require("../assertEqual");
 const head = require("../head");
 
-assertEqual(head(["Lighthouse Labs", "Web Development"]), "Bootcamp");
-assertEqual(head([1,2,3,4,5]), 1);
-assertEqual(head([1]),1);
-assertEqual(head([]), undefined);
+// assertEqual(head(["Lighthouse Labs", "Web Development"]), "Bootcamp");
+// assertEqual(head([1,2,3,4,5]), 1);
+// assertEqual(head([1]),1);
+// assertEqual(head([]), undefined);
+
+const assert = require('chai').assert;
+// const head = require('../head');
+
+describe("#head", () => {
+  it("returns 1 for [1, 2, 3]", () => {
+    assert.strictEqual(head([1, 2, 3]), 1);
+  });
+});
+
+it("returns '5' for ['5']", () => {
+  assert.strictEqual(head(['5']), '5');
+});
