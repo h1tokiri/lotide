7d66255c715a4ab0adaa04035a738da9646c7937
Javin Leslie Lau <javinlau21@gmail.com>
2024-11-11 21:49:16 -0500

    without.js 112149Nov24

--- without.js
+++ without.js
@@ -56,3 +56,5 @@ const words = ["hello", "world", "lighthouse"];
 without(words, ["lighthouse"]); // no need to capture return value for this test case
 // Make sure the original array was not altered by the without function
 assertArraysEqual(words, ["hello", "world", "lighthouse"]); // this returns as assertion passed because i don't include the without in here.
+
+module.exports = without;
