4b732fef01d3552f0d35f3121e67549850ade2db
Javin Leslie Lau <javinlau21@gmail.com>
2024-11-11 22:02:21 -0500

    index.js 112202Nov24

--- index.js
+++ index.js
@@ -11,7 +11,9 @@ const countOnly = require('./countOnly');
 const eqObjects = require('./eqObjects');
 const findKey = require('./findKey');
 const findKeyByValue = require('./findKeyByValue');
+const flatten = require('./flatten');
 const letterPositions = require('./letterPositions');
+const map = require('./map');
 const takeUntil = require('./takeUntil');
 const without = require('./without');
 
@@ -29,7 +31,9 @@ module.exports = {
   eqObjects: eqObjects,
   findKey: findKey,
   findKeyByValue: findKeyByValue,
+  flatten: flatten,
   letterPositions: letterPositions,
+  map: map,
   takeUntil: takeUntil,
   without: without
 };
