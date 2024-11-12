37d6f70843898b7af952b6c652cec0d5e82e7959
Javin Leslie Lau <javinlau21@gmail.com>
2024-11-11 21:54:00 -0500

    index.js 112153Nov24

--- index.js
+++ index.js
@@ -5,6 +5,18 @@ const middle = require('./middle');
 const assertArraysEqual = require('./assertArraysEqual');
 const assertEqual = require('./assertEqual');
 const eqArrays = require('./eqArrays');
+const assertObjectsEqual = require('./assertObjectsEqual');
+const countLetters = require('./counterLetters');
+const countOnly = require('./countOnly');
+const eqObjects = require('./eqObjects');
+const findKey = require('./findKey');
+const findKeyByValue = require('./findKeyByValue');
+const flatten = require('./flatten');
+const letterPositions = require('./letterPositions');
+const map = require('./map');
+const takeUntil = require('./takeUntil');
+const without = require('./without');
+
 
 module.exports = {
   head:   head,
@@ -13,4 +25,15 @@ module.exports = {
   assertArraysEqual: assertArraysEqual,
   assertEqual: assertEqual,
   eqArrays: eqArrays,
+  assertObjectsEqual: assertObjectsEqual,
+  counterLetters: countLetters,
+  countOnly: countOnly,
+  eqObjects: eqObjects,
+  findKey: findKey,
+  findKeyByValue: findKeyByValue,
+  flatten: flatten,
+  letterPositions: letterPositions,
+  map: map,
+  takeUntil: takeUntil,
+  without: without
 };
